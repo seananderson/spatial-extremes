d2be876ea1b30474800b391407539a0ba2734608
Sean Anderson <sean@seananderson.ca>
2015-10-20 11:25:12 -0700

    Initial commit

---
+++ 0-read.R
@@ -0,0 +1,29 @@
+library("dplyr")
+
+if (!file.exists("generated-data/rf-haul-2003-2014.rds")) {
+  haul <- readxl::read_excel("data/RockfishHaulCatchIndivData2003To2014_20150826Fnl.xlsx",
+    sheet = 2, skip = 8) %>%
+    as.data.frame()
+  names(haul) <- tolower(names(haul))
+  names(haul) <- sub(" ", "_", names(haul))
+  haul <- mutate(haul, yr = as.numeric(sub("Cycle ", "", survey_cycle))) %>%
+    rename(
+      haul_latitude = `haul_latitude (decimal degrees)`,
+      haul_longitude = `haul_longitude (decimal degrees)`,
+      haul_depth = `haul_depth (meters)`,
+      trawl_duration = `trawl_duration (hours)`,
+      area_swept = `area_swept by the net (hectares)`)
+  saveRDS(haul, file = "generated-data/rf-haul-2003-2014.rds")
+}
+
+if (!file.exists("generated-data/rf-ind-2003-2014.rds")) {
+  ind <- readxl::read_excel("data/RockfishHaulCatchIndivData2003To2014_20150826Fnl.xlsx",
+    sheet = 3, skip = 7) %>%
+    as.data.frame()
+  names(ind) <- tolower(names(ind))
+  ind <-  mutate(ind, yr = as.numeric(sub("Cycle ", "", project_cycle)))
+  saveRDS(ind, file = "generated-data/rf-ind-2003-2014.rds")
+}
+
+haul <- readRDS("generated-data/rf-haul-2003-2014.rds")
+ind <- readRDS("generated-data/rf-ind-2003-2014.rds")
