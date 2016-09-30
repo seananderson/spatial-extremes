5df2f4836356cd1bc63a72d09faa5335173edef3
eric-ward <eric.ward@noaa.gov>
2016-09-29 17:17:39 -0700

    added script for blooms file
    Former-commit-id: 283f66f460a753f3158701ddedec1a7193d5e81e

--- examples/modis_chl/blooms.R
+++ examples/modis_chl/blooms.R
@@ -46,3 +46,40 @@ filter(d, prc_chla_trans < 3, prc_chla_trans > -3) %>%
 # convert to UTMs
 # sample grid - at ?
 # fiddle with downsampling (500?), fiddle with knots (30-50)
+
+# try downsampling of every 20th lat/lon
+dowsample=20
+lons = unique(d$lon)[seq(1, length(unique(d$lon)), dowsample)]
+lats = unique(d$lat)[seq(1, length(unique(d$lat)), dowsample)]
+dsub = d[which(d$lat%in% lats & d$lon %in% lons),]
+
+dsub = dsub[which(is.na(dsub$prc_chla_trans)==FALSE),]
+
+nKnots = 75
+knots = cluster::pam(dsub[,c("lon","lat")],nKnots)$medoids
+filter(dsub, prc_chla_trans < 3, prc_chla_trans > -3) %>%
+  ggplot() + geom_point(data=dsub, aes(lon, lat, color = prc_chla_trans)) +
+  facet_wrap(~month)
+distKnots = as.matrix(dist(knots))
+distKnotsSq = distKnots^2 # squared distances
+
+# Calculate distance from knots to grid
+distAll = as.matrix(dist(rbind(dsub[,c("lon","lat")], knots)))^2
+# this is the transpose of the lower left corner
+nLocs = nrow(dsub)
+distKnots21Sq = t(distAll[-c(1:nLocs), -c((nLocs+1):ncol(distAll))])
+
+Y = as.numeric(dsub$prc_chla_trans)
+yearID = as.numeric(as.factor(dsub$month))
+stationID = seq(1,nrow(dsub))
+
+# create list for STAN
+spatglm_data = list("nKnots"=nKnots, "nLocs"=nLocs, "nT" = length(unique(yearID)), "N" = length(Y), "stationID" = stationID, "yearID" = yearID, "y" = Y, "distKnotsSq" = distKnotsSq, "distKnots21Sq" = distKnots21Sq, "x" = rep(0, length(Y)))
+spatglm_pars = c("scaledf","yearEffects", "CV", "gp_sigmaSq", "gp_scale", "year_sigma","ar","B","spatialEffectsKnots")
+
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+# estimate model. This model is modified from the simulation model by (1) including indices to allow NAs in the inputted data, and (2) including estimated year effects (intercepts)
+stanMod_gamma = stan(file = 'stan_models/mvtGamma_estSigma_index_cov_yr_ar1.stan',data = spatglm_data,
+  verbose = TRUE, chains = 3, thin = 1, warmup = 1000, iter = 2000, pars = spatglm_pars)
