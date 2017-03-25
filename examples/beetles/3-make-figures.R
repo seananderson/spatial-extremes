9ef87e25464cf938fc25a7d373cd41e4d5f98128
Sean Anderson <sean@seananderson.ca>
2017-03-24 18:39:52 -0700

    Develop map figure, start developing nu density panel
    Former-commit-id: e49bfa3e3ecfaa6f88b76b78a02eb1f16e5d6f5c

--- examples/beetles/3-make-figures.R
+++ examples/beetles/3-make-figures.R
@@ -4,6 +4,7 @@ library(rrfields)
 library(rstan)
 library(viridis)
 library(assertthat)
+library(ggsidekick) # devtools::install_github("seananderson/ggsidekick")
 
 id <- "mountain-pine-beetle-pnw-raster"
 nbin <- 500
@@ -34,45 +35,64 @@ mpc <- ggplot2::map_data("worldHires", "Canada")
 mps <- ggplot2::map_data("state")
 mpc$group <- mpc$group + max(mps$group)
 mp <- rbind(mpc,mps)
-
-# ggplot(mp, aes(long,lat, group=group)) +
-#   geom_polygon(fill = NA, colour = "grey50") +
-#   coord_equal(xlim = c(-150, -80), ylim = c(20, 70))
 mp2 <- mp
 coordinates(mp2) <- c("long", "lat")
 proj <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
 proj4string(mp2) <- CRS("+proj=longlat +datum=NAD83")
-proj4string(mp2)
 mp2 <- spTransform(mp2, CRS(proj))
-proj4string(mp2)
-
 mp2 <- fortify(as.data.frame(mp2))
 
-ggplot(filter(d_highres, year == 2010), aes(x, y)) +
-  geom_polygon(data = mp2, aes(long, lat, group = group),
+# --------------------
+# This creates a map of Washington and Oregon State:
+margin_gap_y <- c(-2, 2)
+margin_gap_x <- c(-1, 2)
+
+ggplot(filter(d_highres, year == 2010), aes(x/1e5, y/1e5)) +
+  geom_polygon(data = mp2, aes(long/1e5, lat/1e5, group = group),
   inherit.aes = FALSE, fill = "white", col = "grey40", lwd = 0.3) +
-  geom_point(size = 0.1, alpha = 0.1) +
-  facet_wrap(~year) +
+  geom_point(size = 0.1, alpha = 0.2) +
+  # facet_wrap(~year) +
+  theme_sleek()+
   theme(panel.background = element_rect(fill="#e6e8ed")) +
-  coord_fixed(xlim = range(d_highres$x), ylim = range(d_highres$y))
+  coord_fixed(xlim = range(d_highres$x)/1e5 + margin_gap_x, ylim = range(d_highres$y)/1e5 + margin_gap_y) +
+  xlab(expression(10^5~UTM~West)) +
+  ylab(expression(10^5~UTM~North))
+ggsave("figs/map.pdf", width = 4, height = 5)
+
+# --------------------
+# Create a panel showing the nu parameter from the mvt model
+
+e <- extract(mvt$model)
+
+prior <- data.frame(df = rgamma(1e6, shape = 2, rate = 0.1))  %>% 
+  filter(df > 2)
+
+ggplot(data.frame(df = e$df), aes(df)) +
+  geom_density() +
+  theme_sleek() +
+  geom_density(data = prior, aes(df), inherit.aes = FALSE) +
+  scale_x_continuous(limits = c(2, 20))
 
 
+posterior <- density(e$df, cut = 0, from = 2, to = 20)
+# hist(e$df, probability = TRUE)
+plot(0,0, xlim = c(2, 10), ylim = c(0, 0.7),type = "n")
+lines(posterior)
+lines(density(prior$df, cut = 0, from = 2, to = 10), col = "grey40",
+  lty = 2)
+
+
+
+
+# --------------------
+# Make the main spatiotemporal prediction plot for the 
+# MVT random fields model 
 pred <- predict(mvt, interval = "confidence", conf_level = 0.95,
   newdata = d)
 pred <- pred %>% mutate(x = d$x, y = d$y, observed = d$cover,
   year = d$year, residual = log(observed) - estimate)
 
-g <- ggplot(dplyr::filter(pred),
-  aes(x, y, fill = exp(estimate))) +
-  geom_tile() + facet_wrap(~year) +
-  scale_fill_viridis(option = "D", trans = "sqrt", breaks = seq(0.1, 1.3, 0.3)) +
-  coord_fixed() +
-  ylab(expression(10^5~UTM~North)) +
-  xlab(expression(10^5~UTM~West)) +
-  labs(fill = "Beetle\n% cover")
-
-print(g)
-
+# This creates a data frame to label the years:
 yr <- unique(select(d, year))
 yr <- mutate(yr, x = -17, y = 24)
 
