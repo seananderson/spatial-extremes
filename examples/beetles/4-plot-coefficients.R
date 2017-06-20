02f13f375eaf0a5e1840b92f421c6d3b7dbede0b
Sean Anderson <sean@seananderson.ca>
2017-06-20 10:04:23 -0700

    Start file to plot beetle coefficient comparison
    Former-commit-id: bb7387335e9579a84a31e40dea89e6a1d515c746

---
+++ examples/beetles/4-plot-coefficients.R
@@ -0,0 +1,31 @@
+# Plot the model confessions from the 2 models
+
+rm(list = ls())
+load("examples/beetles/mountain-pine-beetle-pnw-raster-mvn-lognormal-500x500.rda")
+load("examples/beetles/mountain-pine-beetle-pnw-raster-mvt-lognormal-500x500.rda")
+
+ls()
+e <- extract(mvt$model)
+names(mvt$model)
+print(mvt)
+library(dplyr)
+
+library(broom)
+posterior_t <- tidy(mvt$model, estimate.method = "median", 
+  conf.int = TRUE, rhat = TRUE, ess = TRUE) %>%
+  filter(!(grepl("log_lik", term) | grepl("spatialEffects", term) | grepl("yearEffects", term)))
+
+posterior_n <- tidy(mvn$model, estimate.method = "median", 
+  conf.int = TRUE, rhat = TRUE, ess = TRUE) %>%
+  filter(!(grepl("log_lik", term) | grepl("spatialEffects", term) | grepl("yearEffects", term)))
+
+posteriors <- bind_rows(data.frame(posterior_t, model = "MVT", stringsAsFactors = FALSE), 
+  data.frame(posterior_n, model = "MVN", stringsAsFactors = FALSE))
+head(posteriors)
+
+library(ggplot2)
+
+print(posteriors)
+ggplot(posteriors, aes(term, estimate, ymin = conf.low, ymax = conf.high, color = model)) +
+  geom_pointrange()
+  
